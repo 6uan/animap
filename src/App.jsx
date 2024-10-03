0c154bb73ea931fe28fb2a7131a6f0ef7859752e
6uan <6uannn@gmail.com>
2024-10-02 23:39:39 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useRef, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+
+import "mapbox-gl/dist/mapbox-gl.css";
+
+function App() {
+  const mapRef = useRef();
+  const mapContainerRef = useRef();
+
+  useEffect(() => {
+    mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+    });
+
+    return () => {
+      mapRef.current.remove();
+    };
+  }, []);
+
+  return (
+    <>
+      <div id="map-container" ref={mapContainerRef} />
+    </>
+  );
+}
+
+export default App;
